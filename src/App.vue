b1176bbc8e3590826f51e169d4743ef8fced3cbb
Iago Borba de Almeida <iago.borba.almeida@gmai.com>
2021-10-12 16:01:56 -0300

    first commit ful

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="progressWrapper bg-light bg-gradient">
+    <div class="progress bg-primary bg-gradient" :style="`width:${progress}%`"></div>
+  </div>
+  <TypingBox :done="text.done" :pending="text.pending"/>
+  <div class="mx-5 row counter">
+    <div class="col card">
+      <div class="card-body row shadow-sm" data-bs-toggle="collapse" data-bs-target="#collapse_info">
+        <div class="col d-flex justify-content-center align-items-center flex-column">
+          <h3 class="text-success mb-1">{{text.corrects}}</h3>
+          <small class="text-muted">Acertos</small>
+        </div>
+        <div class="col d-flex justify-content-center align-items-center flex-column">
+          <h3 class="text-danger mb-1">{{text.wrongs}}</h3>
+          <small class="text-muted">Erros</small>
+        </div>
+        <div class="col d-flex justify-content-center align-items-center flex-column">
+          <h3 class="text-primary mb-1">{{((text.corrects/(text.wrongs+text.corrects))*100).toFixed(2) || '-'}}<small>%</small></h3>
+          <small class="text-muted">Precisão</small>
+        </div>
+      </div>
+      <div class="card-body row shadow-sm mt-3 collapse" id="collapse_info" style="overflow-y: auto; max-height:300px;">
+        <div class="col">
+          <b>Acertos</b>
+          <ul>
+            <li v-for="correct, correct_index in metrics.corrects" :key="`correct_${correct_index}`">
+              <b class="mr-1">{{correct_index}}</b>
+              {{correct}}
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <b>Erros</b>
+          <ul>
+            <li v-for="wrong, wrong_index in metrics.wrongs" :key="`wrong_${wrong_index}`">
+              <b class="mr-1">{{wrong_index}}</b>
+              {{wrong}}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TypingBox from './components/TypingBox.vue'
+import { poems } from './assets/texts.json';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TypingBox
+  },
+  data: () => ({
+    text: {
+      done: [
+        ''
+      ],
+      pending: poems[0],
+      wrongs:   0,
+      corrects: 0
+    },
+    metrics: {
+      wrongs: {},
+      corrects: {}
+    },
+    ignoreKeys: [
+      'CapsLock', 'Shift', 'Control', 'Backspace'
+    ]
+  }),
+  computed: {
+    progress: function () {
+      let pending = this.text.pending.reduce((acc, line) => (acc + line.length), 0);
+      let done = this.text.done.reduce((acc, line) => (acc + line.length), 0);
+      let total = pending + done;
+      return (done*100/total).toFixed(0);
+    }
+  },
+  created: function() {
+    document.addEventListener('keydown', (event) => {
+      let key = event.key;
+      if(this.ignoreKeys.includes(key)) { return; }
+
+      let currentDone = this.text.done.length - 1;
+      let currentLine = this.text.pending[0];
+      let correct = currentLine.length > 0 ? currentLine[0] : 'Enter';
+      
+      if(key == correct) {
+        this.text.done[currentDone] += correct != 'Enter' ? correct : '';
+        this.text.corrects += 1;
+        this.text.pending[0] = currentLine.substring(1, currentLine.length);
+        if(correct == 'Enter') {
+          this.text.pending.shift();
+          this.text.done.push('');
+        }else{
+          this.metrics.corrects[key.toUpperCase()] = this.metrics.corrects[key.toUpperCase()] ? this.metrics.corrects[key.toUpperCase()] + 1 : 1;
+      
+        }
+      }else{
+        if(key != 'Enter'){
+          this.metrics.wrongs[key.toUpperCase()] = this.metrics.wrongs[key.toUpperCase()] ? this.metrics.wrongs[key.toUpperCase()] + 1 : 1;
+        }
+        this.text.wrongs += 1;
+      }
+    })
   }
 }
 </script>
---
+++ src/components/TypingBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex justify-content-center aligns-item-center flex-column">
+        <span class="text-primary" v-for="done_line, done_index in done.slice(0, done.length-1)" :key="`done_line_${done_index}`">
+            {{ done_line }}
+        </span>
+        <span>
+            <span class="text-primary">{{ done[done.length - 1] }}</span> <b class="blink">></b> {{ pending[0] }}
+        </span>
+        <span class="text-muted" v-for="pending_line, pending_index in pending.slice(1, pending.length)" :key="`pending_line_${pending_index}`">
+            {{ pending_line }}
+        </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TypingBox',
+  props: {
+      done: { type: Array },
+      pending: { type: Array }
+  }
+}
+</script>
+
+<style scoped>
+    .blink {
+        animation: blink 250ms ease-in-out infinite alternate-reverse;
+    }
+    @keyframes blink {
+        0%{
+            opacity: 1;
+        }
+        100%{
+            opacity: 0.5;
+        }
+    }
+</style>
